7c92f3b714d028febd6c5fce8dd768de42eeb669
John Poulson <Jdpoulson@gmail.com>
2018-11-27 14:43:07 -0800

    created a new triangle tracker website

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($("#add1").val());
+    var side2 = parseInt($("#add2").val());
+    var side3 = parseInt($("#add3").val());
+
+    if ((side1 === side2) && (side1 === side3) && (side2 === side3)) {
+      $(".equilateral").show();
+      $(".isosceles").hide();
+      $(".scalene").hide();
+      $(".not-a-triangle").hide();
+    };
+
+    if (((side1 === side2) && (side1 > side3) && (side2 > side3)) || ((side1 === side3) && (side1 > side2) && (side3 > side2)) || ((side2 === side3) && (side2 > side1) && (side3 > side1))) {
+      $(".equilateral").hide();
+      $(".isosceles").show();
+      $(".scalene").hide();
+      $(".not-a-triangle").hide();
+    };
+
+    if (((side1 > side2) && (side2 > side3) && (side1 > side3)) || ((side2 > side1) && (side3 > side1) && (side2 > side3)) || ((side1 > side2) && (side3 > side2) && (side1 > side3)) || ((side3 > side1) && (side3 > side2) && (side2 > side1)) || ((side3 > side1) && (side3 > side2) && (side1 > side2))) {
+      $(".equilateral").hide();
+      $(".isosceles").hide();
+      $(".scalene").show();
+      $(".not-a-triangle").hide();
+    };
+
+    if (((side1 === side2) && (side3 > side1) && (side3 > side2)) || ((side1 === side3) && (side2 > side1) && (side2 > side3)) || ((side2 === side3) && (side1 > side2) && (side1 > side3)) || ((side1 === 0) || (side2 === 0) || (side3 === 0))) {
+      $(".equilateral").hide();
+      $(".isosceles").hide();
+      $(".scalene").hide();
+      $(".not-a-triangle").show();
+    };
+  });
+});
